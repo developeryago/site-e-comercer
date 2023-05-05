83e822f1e0d4437381a85f13df9073e7908e0d12
developeryago <contato.yagomachado@outlook.com>
2023-05-05 10:06:05 -0300

    criação de slides

--- script/script.js
+++ script/script.js
@@ -6,4 +6,19 @@ function clickMenu() {
     } else {
          itens.style.display = 'flex'
     }
- } 
+}
+
+let cont = 1
+document.getElementById("radio1").checked = true
+
+setInterval( function(){
+     nextImage()
+}, 7000)
+
+function nextImage(){
+     cont += 1 
+     if (cont > 3){
+          cont = 1
+     }
+     document.getElementById("radio" + cont).checked = true
+}
